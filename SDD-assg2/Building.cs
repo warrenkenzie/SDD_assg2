44a9bf8faf2bd0da1c010e384f64abc7315f2b33
warrenkenzie <warren.kenzie28@gmail.com>
2023-12-22 11:52:27 +0800

    made a Building class

---
+++ SDD-assg2/Building.cs
@@ -0,0 +1,22 @@
+﻿namespace SDD_assg2
+{
+    class Building
+    {
+        // name of buidling
+        public string BuildingName { get; set; }
+
+        // short name of buidling shown on board
+        public string BuildingAcronym { get; set; }
+
+        public Building()
+        {
+
+        }
+
+        public Building(string buildingName, string buildingAcronym)
+        {
+            BuildingName = buildingName;
+            BuildingAcronym = buildingAcronym;
+        }
+    }
+}
